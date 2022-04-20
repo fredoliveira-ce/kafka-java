5e1979c41d0c414f181388b416c908a2df5370b2
Fred Junior <fredoliveira.ce@gmail.com>
2022-04-20 21:23:45 +0200

    First message produced.

--- application/src/main/java/nl/oliveira/kafka/Application.java
+++ application/src/main/java/nl/oliveira/kafka/Application.java
@@ -1,13 +1,33 @@
 package nl.oliveira.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class.getSimpleName());
 
     public static void main(String[] args) {
-        log.info("Hello");
+        log.info("Producing message");
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        final ProducerRecord<String, String> record = new ProducerRecord<>("first_java_topic", "hello");
+
+        producer.send(record);
+        producer.flush();
+        producer.close();
     }
 }

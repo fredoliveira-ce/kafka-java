013276529b1f34ab0d73332f9fe3da347437affd
Fred Junior <fredoliveira.ce@gmail.com>
2022-04-20 21:00:00 +0200

    Hello world and git ignore.

---
+++ application/src/main/java/nl/oliveira/kafka/Application.java
@@ -0,0 +1,13 @@
+package nl.oliveira.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello");
+    }
+}

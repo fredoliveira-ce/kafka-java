cd95f0e6304d9ce684d8b3cca85357a7a298ab3e
Fred Junior <fredoliveira.ce@gmail.com>
2022-04-29 15:07:49 +0200

    Make the producer more efficient

--- wikimedia/src/main/java/nl/oliveira/kafka/WikimediaChangesProducer.java
+++ wikimedia/src/main/java/nl/oliveira/kafka/WikimediaChangesProducer.java
@@ -9,8 +9,11 @@ import java.net.URI;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import static org.apache.kafka.clients.producer.ProducerConfig.BATCH_SIZE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.COMPRESSION_TYPE_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.LINGER_MS_CONFIG;
 import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
 
 public class WikimediaChangesProducer {
@@ -20,6 +23,9 @@ public class WikimediaChangesProducer {
         properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(LINGER_MS_CONFIG, "20");
+        properties.setProperty(BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+        properties.setProperty(COMPRESSION_TYPE_CONFIG, "snappy");
 
         final KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 

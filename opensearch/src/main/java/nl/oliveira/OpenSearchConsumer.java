df10b82a682a728186369dce19c09bf402d5ced3
Fred Junior <fredoliveira.ce@gmail.com>
2022-05-05 19:56:59 +0200

    Consuming wikimedia.

--- opensearch/src/main/java/nl/oliveira/OpenSearchConsumer.java
+++ opensearch/src/main/java/nl/oliveira/OpenSearchConsumer.java
@@ -1,21 +1,38 @@
 package nl.oliveira;
 
+import com.google.gson.JsonParser;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.GROUP_ID_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 
 public class OpenSearchConsumer {
     public static void main(String[] args) throws IOException {
@@ -23,7 +40,9 @@ public class OpenSearchConsumer {
 
         final var client = createOpenSearchClient();
 
-        try (client) {
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
+
+        try (client; consumer) {
             final boolean hasIndex = client.indices().exists(new GetIndexRequest("wikimedia"), RequestOptions.DEFAULT);
 
             if (!hasIndex) {
@@ -34,7 +53,50 @@ public class OpenSearchConsumer {
                 log.info("Wikimedia already created");
             }
 
+            consumer.subscribe(Collections.singletonList("wikimedia.recent-change"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+                log.info("Received {} records", records.count());
+
+                for (ConsumerRecord<String, String> record : records) {
+                    final var id = extractId(record.value());
+
+                    IndexRequest indexRequest = new IndexRequest("wikimedia")
+                            .source(record.value(), XContentType.JSON)
+                            .id(id);
+
+                    IndexResponse response = client.index(indexRequest, RequestOptions.DEFAULT);
+
+                    log.info("Key: {}, value: {} ", record.key(), record.value());
+                    log.info("Partition: {}, Offset: {}",record.partition(), record.offset());
+                    log.info(response.getId());
+                }
+
+            }
+
         }
+    }
+
+    private static String extractId(String record) {
+        //id = record.topic() + record.partition() + record.offset()
+        return JsonParser.parseString(record)
+                .getAsJsonObject()
+                .get("meta")
+                .getAsJsonObject()
+                .get("id")
+                .getAsString();
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(GROUP_ID_CONFIG, "opensearch-consumer-group");
+        properties.setProperty(AUTO_OFFSET_RESET_CONFIG, "latest");
+
+        return new KafkaConsumer<>(properties);
     }
 
     private static RestHighLevelClient createOpenSearchClient() {
